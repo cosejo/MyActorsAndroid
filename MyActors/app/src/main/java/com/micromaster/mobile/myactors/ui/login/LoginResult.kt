a01582f71106495a62e79ea57b89ca720e33c446
Carlos Osejo <caosejo@gmail.com>
2021-07-24 18:19:48 -0600

    Adding MVVM to Fake Login Feature

---
+++ MyActors/app/src/main/java/com/micromaster/mobile/myactors/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.micromaster.mobile.myactors.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
